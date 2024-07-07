5a54909fd7292731334137416ba82def75997aec
Hashen Kottegoda <thkottegoda@gmail.com>
2024-07-07 16:57:17 +0530

    added test for getAdjacentMines

--- src/test/java/com/mineSweeper/app/GridTest.java
+++ src/test/java/com/mineSweeper/app/GridTest.java
@@ -21,4 +21,28 @@ public class GridTest {
         }
         assertEquals(3, mineCount);
     }
+
+    @Test
+    public void testAdjacentMines() {
+        Grid grid = new Grid(6, 0);
+        grid.getSquare(0, 0).setMine(true);
+        grid.getSquare(2, 0).setMine(true);
+        grid.getSquare(1, 1).setMine(true);
+        grid.getSquare(2, 1).setMine(true);
+        grid.getSquare(3, 1).setMine(true);
+        grid.getSquare(2, 2).setMine(true);
+        grid.getSquare(0, 3).setMine(true);
+        grid.getSquare(4, 3).setMine(true);
+
+        assertEquals(4, grid.getSquare(1, 0).getAdjacentMines());
+        assertEquals(3, grid.getSquare(3, 0).getAdjacentMines());
+        assertEquals(2, grid.getSquare(0, 1).getAdjacentMines());
+        assertEquals(0, grid.getSquare(5, 1).getAdjacentMines());
+        assertEquals(4, grid.getSquare(3, 2).getAdjacentMines());
+        assertEquals(2, grid.getSquare(1, 3).getAdjacentMines());
+        assertEquals(1, grid.getSquare(0, 4).getAdjacentMines());
+        assertEquals(0, grid.getSquare(2, 4).getAdjacentMines());
+
+    }
+
 }
