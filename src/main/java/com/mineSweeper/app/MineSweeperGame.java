a046eb1ae9d327c21cac5bcf521334607420e768
Hashen Kottegoda <thkottegoda@gmail.com>
2024-07-07 22:49:36 +0530

    improved program's outputs

--- src/main/java/com/mineSweeper/app/MineSweeperGame.java
+++ src/main/java/com/mineSweeper/app/MineSweeperGame.java
@@ -1,20 +1,21 @@
 package com.mineSweeper.app;
+
 import java.util.Scanner;
 
 
 public class MineSweeperGame {
     private Grid grid;
     private static final int MIN_GRID_SIZE = 2;
-    private static final int MAX_GRID_SIZE = 9;
+    private static final int MAX_GRID_SIZE = 15;
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
         int gridSize;
         int mineCount;
 
-        System.out.println(" ");
+        System.out.println();
         System.out.println("Welcome to Minesweeper!");
-        System.out.println(" ");
+        System.out.println();
         do {
             System.out.println("Enter the size of the grid between " + MIN_GRID_SIZE + " and " + MAX_GRID_SIZE + " (e.g. 4 for a 4x4 grid): ");
             gridSize = getIntInput(scanner);
@@ -22,38 +23,39 @@ public class MineSweeperGame {
 
         int maxMineCount = (int) (gridSize * gridSize * 0.35);
         do {
-            System.out.println("Enter the number of mines to place on the grid (maximum is " + maxMineCount + "): ");
+            System.out.println("Enter the number of mines to place on the grid (maximum is 35% of the total squares = " + maxMineCount + "): ");
             mineCount = getIntInput(scanner);
         } while (mineCount < 1 || mineCount > maxMineCount);
         grid = new Grid(gridSize, mineCount);
 
+        printGrid(false);
         while (true) {
-            printGrid();
-            System.out.println("Select a square to reveal (e.g. A1): ");
+            System.out.print("Select a square to reveal (e.g. A1): ");
             String input = scanner.next();
             if (!isSquareWithinValidRange(input, gridSize)) {
                 System.out.println("Incorrect input");
                 continue;
             }
             int row = input.charAt(0) - 'A';
-            int col = Character.getNumericValue(input.charAt(1)) - 1;
+            int col = Integer.parseInt(input.substring(1)) - 1;
 
             if (grid.getSquare(row, col).isMine()) {
                 System.out.println("Oh no, you detonated a mine! Game over.");
                 break;
             } else {
                 revealSquare(row, col);
+                System.out.println("This square contains " + grid.getSquare(row, col).getAdjacentMines() + " adjacent mines.");
+                printGrid(true);
+
                 if (checkWin()) {
                     System.out.println("Congratulations, you have won the game!");
                     break;
                 }
             }
         }
-
-
         System.out.println("Press any key to play again...");
         scanner.next();
-
+        start();
     }
 
     private int getIntInput(Scanner scanner) {
@@ -65,13 +67,16 @@ public class MineSweeperGame {
     }
 
     private boolean isSquareWithinValidRange(String input, int gridSize) {
-        if (input.length() != 2) return false;
+        if (input.length() < 2 || input.length() > 3) return false;
         char row = input.charAt(0);
-        char col = input.charAt(1);
-        return row>='A' && row<'A'+gridSize && col>='1' && col<'1'+gridSize;
+        int col = Integer.parseInt(input.substring(1));
+        return row >= 'A' && row < 'A' + gridSize && col >= 1 && col < 1 + gridSize;
     }
 
-    private void printGrid() {
+    private void printGrid(boolean isUpdated) {
+        System.out.println();
+        String message = isUpdated ? "Here is your updated minefield:" : "Here is your minefield:";
+        System.out.println(message);
         System.out.print("  ");
         for (int i = 1; i <= grid.getSize(); i++) {
             System.out.print(i + " ");
@@ -89,6 +94,7 @@ public class MineSweeperGame {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     private void revealSquare(int row, int col) {
