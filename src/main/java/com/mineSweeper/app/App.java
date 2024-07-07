8b6dde1337c410a077536dcb7428b03217b07508
Hashen Kottegoda <thkottegoda@gmail.com>
2024-07-07 13:26:40 +0530

    Initialized maven project

---
+++ src/main/java/com/mineSweeper/app/App.java
@@ -0,0 +1,13 @@
+package com.mineSweeper.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
